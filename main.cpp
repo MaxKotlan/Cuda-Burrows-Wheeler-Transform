0df0c9b027f5dca5057f4d41d3c05576b69f57b6
Max Kotlan <max.maxkot@gmail.com>
2020-03-27 19:41:00 -0400

    bwt on lotr

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
 #include "bwt.h"
+
+std::vector<unsigned char> readFileIntoBuffer(std::string filename){
+    	FILE* file = fopen(filename.c_str(), "rb");
+        fseek(file, 0, SEEK_END);
+		int length = ftell(file)+1;
+		fseek(file, 0, SEEK_SET);
+        std::vector<unsigned char> buffer(length);
+		fread(buffer.data(), buffer.size(), 1, file);
+        return std::move(buffer);
+}
 
 int main(int argc, char** argv){
     std::vector<unsigned char> data;
@@ -13,5 +24,11 @@ int main(int argc, char** argv){
     std::cout << t.originalIndex << std::endl;
     for (auto c : t.data)
         std::cout << c;
+
+    std::vector<unsigned char> lotr = readFileIntoBuffer("lotr.txt");
+    BWT::TransformedData lotr_t = BWT::BWT(lotr);
+    for (auto c : lotr_t.data)
+        std::cout << c;
+
 
 }
