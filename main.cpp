c476c7dbdb8921cf87258a3a1ef46ea9308ec7f5
Max Kotlan <max.maxkot@gmail.com>
2020-03-27 00:43:25 -0400

    Seems to be almost working. One or two characters out of place

--- main.cpp
+++ main.cpp
@@ -3,50 +3,67 @@
 #include <algorithm>
 
 
+void printMatrix(std::vector<unsigned char>& data){
+     for (int i = 0; i < data.size(); i++){
+        for(int j = 0; j < data.size(); j++)
+            std::cout << data[(j - i)%(data.size())];
+        std::cout << std::endl;
+    }   
+}
+
+void printNthColumn(std::vector<unsigned char>& data, unsigned int column){
+    for (int i = 0; i < data.size(); i++){
+        std::cout << data[(column - i)%(data.size())] << std::endl;
+    }
+}
+
+std::vector<int> sortIndex(std::vector<unsigned char>& data){
+    std::vector<int> indices(data.size());
+    for (int i = 0; i < indices.size(); i++)
+        indices[i] = i;
+
+    std::sort(indices.begin(), indices.end(), [&](int a, int b){
+        int resa = 0; int resb = 0;
+        for (int i = 0; i < data.size() && resa == resb; i++){
+            resa += data[(i - a)%(data.size())]+ (INT_MAX - sizeof(unsigned char)) >> i;
+            resb += data[(i - b)%(data.size())]+ (INT_MAX - sizeof(unsigned char)) >> i;
+        }
+        return resa < resb;
+    } );
+
+    return std::move(indices);
+}
 
 void burrowswheeler(std::vector<unsigned char>& data){
-    
-    std::vector<unsigned char> buffer(data.size()*data.size());
-    std::vector<unsigned int> index(data.size());
+    //std::vector<unsigned int> index(data.size());
+    //printMatrix(data);
+    //printNthColumn(data, 0);
+    std::vector<int> indices = sortIndex(data);
+    //for (int i = 0; i < indices.size(); i++)
+    //    indices[i] = i;
+
+    /* for (int i = 0; i < data.size(); i++){
+        for(int j = 0; j < data.size(); j++)
+            std::cout << data[(j - indices[i])%(data.size())];
+        std::cout << std::endl;
+    }*/
+    std::cout << std::endl << "Transform: ";
     for (int i = 0; i < data.size(); i++)
-        index[i] = i;
+        std::cout << data[(-1 - indices[i])%(data.size())];
 
-    for (int j = 0; j < data.size(); j++)
-        for (int i = 0; i < data.size(); i++)
-            buffer[i+j*data.size()] = data[(i-j)%data.size()];
-
-    for (int j = 0; j < data.size(); j++){
-        for (int i = 0; i < data.size(); i++)
-            std::cout << buffer[i+j*data.size()];
-        std::cout << std::endl;
-    }
-
-    for (auto i : index)
-        std::cout << i;
-    std::cout << std::endl;
-
-    std::sort(index.begin(), index.end(), [buffer, data](int i1, int i2){ 
-        int r1 = 0, r2 = 0;
-        for (int i = 0; i < data.size(); i++){
-            r1 += buffer[i1*data.size() + i]+i*10;
-            r2 += buffer[i2*data.size() + i]+i*10; 
-        }
-        return r1 < r2; 
-    });
-
-    std::cout << std::endl;
-    for (auto i : index){
-        for (int j = 0; j < data.size(); j++)
-            std::cout << buffer[i*data.size() + j];
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
 
 }
 
 int main(int argc, char** argv){
 
-    std::vector<unsigned char> data{'4', '2', '1', '1', '2', '2', '3', '1', '4'};
+    std::vector<unsigned char> data;
+
+    
+    for (auto c : "Tomorrow_and_tomorrow_and_tomorrow")
+        if (c != '\0')
+            data.push_back(c);
+    data.push_back('\0');
+
     burrowswheeler(data);
 
 }
